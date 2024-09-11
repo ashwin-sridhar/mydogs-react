fb476b0cd99578c46ca95bba80e16dbabf2d2f08
Ashwin Sridhar Laptop <ashvin.sriram@gmail.com>
2024-09-11 23:51:43 +0530

    console.log of env

--- src/app/api/dogs/[id]/route.ts
+++ src/app/api/dogs/[id]/route.ts
@@ -8,7 +8,7 @@ export async function GET(
   console.log("Request obj"+request.body);
   console.log("params :"+params);
   const id = params.id
-  console.log("URL"+process.env.FITBARK_REST);
+  console.log("URL"+process.env.NEXT_PUBLIC_FITBARK_REST);
   try {
     const apiRes = await fetch(`${process.env.NEXT_PUBLIC_FITBARK_REST}/dogs/${id}/activity`)
     const data = await apiRes.json()
